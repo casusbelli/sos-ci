b6f408177b00726b5b3e84fb6701194cb7d53910
Silvan Kaiser <silvan@quobyte.com>
2015-07-16 15:39:53 +0200

    Adding script template and yaml file for manual creation of CI test instances

---
+++ sos-ci/run_ci_manually.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#
+# Script for manually creating a CI VM instance with complete DevStack and Settings.
+# Edit to set instance name, also edit ./ansible/manual_ci.yml to adopt to your
+# installation
+
+ansible-playbook ./ansible/manual_ci.yml -e "patch_ref=master instance_name=manualCI"
